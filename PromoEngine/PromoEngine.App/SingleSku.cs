19c57cb3316f16bc0b1e2b8d7ffc97db1f0ceb54
Arun <arun.prakash@live.com>
2020-06-03 16:00:45 +0530

    Define ApplyPromotion on SingleSku type

--- PromoEngine/PromoEngine.App/SingleSku.cs
+++ PromoEngine/PromoEngine.App/SingleSku.cs
@@ -1,6 +1,7 @@
 ﻿using PromoEngine.App.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PromoEngine.App
@@ -23,7 +24,25 @@ namespace PromoEngine.App
 
         public double ApplyPromotion(List<OrderDetail> orderDetails)
         {
-            throw new NotImplementedException();
+            var detail = orderDetails.Where(x => x.Product == this.product).FirstOrDefault();
+
+            if (detail == null) return 0;
+
+            var originalPrice = detail.Quantity * detail.OriginalUnitPrice;
+            this.OriginalPrice = originalPrice;
+
+            if (detail.Quantity >= this.quantity)
+            {
+                var times = detail.Quantity / this.quantity;
+                var rem = detail.Quantity % this.quantity;
+
+                var priceAfterDiscount = (this.promoPrice * times) + (rem * detail.OriginalUnitPrice);
+                this.PriceAfterDiscount = priceAfterDiscount;
+
+                return originalPrice - priceAfterDiscount;
+            }
+            this.PriceAfterDiscount = this.OriginalPrice;
+            return 0;
         }
     }
 }
