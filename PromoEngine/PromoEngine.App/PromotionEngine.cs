057fa8a7349c4c5e8554ff54ad8ef67c6ebbf47a
Arun <arun.prakash@live.com>
2020-06-03 15:28:09 +0530

    Property/ method declaration on PromotionEngine. Initialize PromotionEngine for testing

--- PromoEngine/PromoEngine.App/PromotionEngine.cs
+++ PromoEngine/PromoEngine.App/PromotionEngine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PromoEngine.App.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace PromoEngine.App
 {
     public class PromotionEngine
     {
+        public bool IsPromoApplied { get; set; } = false;
+        public double DiscountedAmount { get; set; } = 0;
+
+        public void ApplyPromotions(List<PromoDefinition> activePromotions, List<OrderDetail> productCart)
+        {
+
+        }
     }
 }
