222477f96d7463ac73be99086ee05fc88bcb39ad
Arun <arun.prakash@live.com>
2020-06-03 15:07:02 +0530

    Initial - Solution skeleton

---
+++ PromoEngine/PromoEngine.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromoEngine.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
