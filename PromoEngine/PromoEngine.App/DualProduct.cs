b336927f65c4437e360c865da54fba40b7d118e6
Arun <arun.prakash@live.com>
2020-06-03 15:38:33 +0530

    Promotions - define properties and parameterized constructors for SingleSku, DualProducts

--- PromoEngine/PromoEngine.App/DualProduct.cs
+++ PromoEngine/PromoEngine.App/DualProduct.cs
@@ -7,9 +7,19 @@ namespace PromoEngine.App
 {
     public class DualProduct : IPromotion
     {
-        public double OriginalPrice => throw new NotImplementedException();
+        private readonly char product1;
+        private readonly char product2;
+        private readonly double promoPrice;
 
-        public double PriceAfterDiscount => throw new NotImplementedException();
+        public double OriginalPrice { get; private set; }
+        public double PriceAfterDiscount { get; private set; }
+
+        public DualProduct(PromoDefinition promo)
+        {
+            this.product1 = promo.ProductList[0];
+            this.product2 = promo.ProductList[1];
+            this.promoPrice = Convert.ToDouble(promo.Value);
+        }
 
         public double ApplyPromotion(List<OrderDetail> orderDetails)
         {
--- PromoEngine/PromoEngine.App/SingleSku.cs
+++ PromoEngine/PromoEngine.App/SingleSku.cs
@@ -7,9 +7,19 @@ namespace PromoEngine.App
 {
     public class SingleSku : IPromotion
     {
-        public double OriginalPrice => throw new NotImplementedException();
+        private readonly char product;
+        private readonly int quantity;
+        private readonly double promoPrice;
 
-        public double PriceAfterDiscount => throw new NotImplementedException();
+        public double OriginalPrice { get; private set; }
+        public double PriceAfterDiscount { get; private set; }
+
+        public SingleSku(PromoDefinition promo)
+        {
+            this.product = promo.ProductList[0];
+            this.quantity = promo.Quantity;
+            this.promoPrice = Convert.ToDouble(promo.Value);
+        }
 
         public double ApplyPromotion(List<OrderDetail> orderDetails)
         {
