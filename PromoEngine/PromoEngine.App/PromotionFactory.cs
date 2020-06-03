12c7ac9c5ac520e947fcb4c39cc80e6c305f3c3d
Arun <arun.prakash@live.com>
2020-06-03 15:34:08 +0530

    Define PromotionFactory class for promotion type creation

---
+++ PromoEngine/PromoEngine.App/PromotionFactory.cs
@@ -0,0 +1,24 @@
+﻿using PromoEngine.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromoEngine.App
+{
+    public class PromotionFactory
+    {
+        public static IPromotion GetFactory(PromoDefinition promoDef)
+        {
+            switch (promoDef.PromoType)
+            {
+                case PromotionTypes.SingleSku:
+                    return new SingleSku();
+                case PromotionTypes.DualProducts:
+                    return new DualProduct();
+                // case Percentage
+                default:
+                    return new SingleSku();
+            }
+        }
+    }
+}
